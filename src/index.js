2d6870c4372f2e3e0fc4ba02894a83bfdf37643a
Pallav987 <pallavvarshney76@gmail.com>
2023-07-06 11:33:50 +0530

    added navbar and sidebar

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import {ContextProvider} from './contexts/ContextProvider';
 
 
 ReactDOM.render(
-    <App />,document.getElementById('root')
+    <ContextProvider>
+        <App />
+    </ContextProvider>
+    ,document.getElementById('root')
 );
